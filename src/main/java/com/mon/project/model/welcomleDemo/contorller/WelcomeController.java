8cb10b1f30736540c419bd557cf10124f013a74b
0MasterShifu0 <1562781705@qq.com>
2019-06-16 16:28:12 +0800

    提交内容:断言异常拦截处理以及自定义增加请求参数 提交时间:2019-06-16 提交人: Master_Shifu_

--- src/main/java/com/mon/project/model/welcomleDemo/contorller/WelcomeController.java
+++ src/main/java/com/mon/project/model/welcomleDemo/contorller/WelcomeController.java
@@ -5,19 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 @RequestMapping("/welcome")
 public class WelcomeController {
 
-//    /**
-//     * hello.html页面
-//     * @return
-//     */
-//    @RequestMapping("welcomeIndex")
-//    public String welcomeIndex(Model model){
-//        return "hello";
-//    }
     /**
      * hello.html页面并输入动态值
      * @return
@@ -31,5 +26,37 @@ public class WelcomeController {
         model.addAttribute("singlePerson",people);
         return "hello";
     }
+
+    /**
+     * 模拟请求接口报错
+     * @return
+     */
+    @RequestMapping("getError")
+    @ResponseBody
+    public List<Welcome> getError(){
+        /**
+         * 测试数据
+         */
+        List<Welcome> welcomes = new ArrayList<>();
+        Welcome w1 = new Welcome();
+        w1.setId(1L);
+        w1.setName("xx1");
+        w1.setAge(11);
+        w1.setGender("女");
+        throw new IllegalArgumentException("抱歉,访问链接有误!");
+//        return welcomes;
+    }
+
+    /**
+     * 模拟请求带断言参数接口
+     * @return
+     */
+    @RequestMapping(value = "getAdvicePost",method = RequestMethod.POST)
+    @ResponseBody
+    public Welcome getAdvicePost(@ModelAttribute("msg") String msg, @RequestBody Welcome w1){
+        w1.setName(msg+w1.getName());
+        return w1;
+    }
+
 
 }
