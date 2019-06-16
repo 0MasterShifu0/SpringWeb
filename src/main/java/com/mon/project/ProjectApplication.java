6d5fcc224c9610746b5190197e0f0ca150b5211a
0MasterShifu0 <1562781705@qq.com>
2019-06-16 18:54:34 +0800

    提交内容:数据库查询配置文件准备以及查询实现 提交时间:2019-06-16 提交人: Master_Shifu_

--- src/main/java/com/mon/project/ProjectApplication.java
+++ src/main/java/com/mon/project/ProjectApplication.java
@@ -1,9 +1,11 @@
 package com.mon.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.mon.project.**.dao"})
 public class ProjectApplication {
 
 	public static void main(String[] args) {
