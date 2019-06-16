632310fac887a2af7fb328e04f3577087a14372a
0MasterShifu0 <1562781705@qq.com>
2019-06-16 15:47:35 +0800

    提交内容:返回动态的Html页面处理 提交时间:2019-06-16 提交人: Master_Shifu_

--- src/main/java/com/mon/project/model/welcomleDemo/contorller/WelcomeController.java
+++ src/main/java/com/mon/project/model/welcomleDemo/contorller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.mon.project.model.welcomleDemo.contorller;
 
+import com.mon.project.model.welcomleDemo.vo.Welcome;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +10,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/welcome")
 public class WelcomeController {
 
+//    /**
+//     * hello.html页面
+//     * @return
+//     */
+//    @RequestMapping("welcomeIndex")
+//    public String welcomeIndex(Model model){
+//        return "hello";
+//    }
     /**
-     * hello.html页面
+     * hello.html页面并输入动态值
      * @return
      */
     @RequestMapping("welcomeIndex")
     public String welcomeIndex(Model model){
+        Welcome people = new Welcome();
+        people.setId(1L);
+        people.setName("Tom");
+        people.setAge(20);
+        model.addAttribute("singlePerson",people);
         return "hello";
     }
 
