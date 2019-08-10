1c20b235b8baf10c280d24edec90009c8cb19f7b
0MasterShifu0 <1562781705@qq.com>
2019-08-10 17:05:19 +0800

    打印错误日志

--- src/main/java/com/mon/project/advice/ExceptionHandlerAdvice.java
+++ src/main/java/com/mon/project/advice/ExceptionHandlerAdvice.java
@@ -1,5 +1,7 @@
 package com.mon.project.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,10 +10,12 @@ import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class ExceptionHandlerAdvice {
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandlerAdvice.class);
 
     //拦截所有Exception,展示Error页面
     @ExceptionHandler(value = Exception.class)
     public String exception(Exception exception, WebRequest webRequest,Model model){
+        logger.error("访问 "+webRequest.getContextPath() +"报错,报错信息为: {}",exception.getMessage());
         model.addAttribute("erroMessage",exception.getMessage());
         return   "error";
     }
